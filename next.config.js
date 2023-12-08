302428e990c0a81db531ce21caf3b92848f57b10
Hamed Bahram <hamed.bahram@gmail.com>
2023-12-08 13:47:51 -0500

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com']
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com'
+      }
+    ]
   }
 }
 
